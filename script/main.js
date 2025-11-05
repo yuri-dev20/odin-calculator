b8ed76f71b281e75b7a80ad766e21acbbed3dbcd
yuri-dev20 <yuriglauco945@gmail.com>
2025-11-05 16:53:05 -0300

    implemented eventListeners and display logic

--- script/main.js
+++ script/main.js
@@ -1,1 +1,25 @@
-console.log("Hello Console.")
+const numbers = document.querySelectorAll(".number");
+const operands = document.querySelectorAll(".operand");
+const clear =  document.querySelector("#clear");
+const backspace =  document.querySelector("#backspace");
+const display = document.querySelector("#display");
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        display.value += number.textContent;
+    });
+});
+
+operands.forEach(operand => {
+    operand.addEventListener("click", () => {
+        display.value += operand.textContent;
+    });
+})
+
+clear.addEventListener("click", () => {
+    display.value = "";
+});
+
+backspace.addEventListener("click", () => {
+    display.value = String(display.value).slice(0, -1);
+})
